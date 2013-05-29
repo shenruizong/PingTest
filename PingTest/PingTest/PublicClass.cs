b244414886e5131078827bf5cb6e01f5a56eeff5
disappeared <shenruizong@gmail.com>
2013-05-29 19:54:59 +0800

    添加publicclass

---
+++ PingTest/PingTest/PublicClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PingTest
+{
+    class PublicClass
+    {
+        public static DatabaseDataSet.ipsDataTable ipsDataTable;
+    }
+}
